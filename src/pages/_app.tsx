c69e32bd2acff7219da6b8c45649e54bca1c112d
jhonatanmizu <natanjesuss20@gmail.com>
2023-01-29 17:03:31 -0300

    :sparkles: added styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
+// Styles
+import GlobalStyles from '@/styles/global'
+// Types
 import type { AppProps } from 'next/app'
+// Components
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Nextjs - boilerplate</title>
+        {/* <link rel="shortcut icon" href="/" /> */}
+        {/* <link rel="apple-touch-icon" href="/" /> */}
+        <meta
+          name="description"
+          content="A simple nextjs boilerplate by Mizugawa"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
